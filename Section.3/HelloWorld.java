97bfd87a12bd503b901c19c724a9fa7b3a1523b9
stomaras <sptomaras@yahoo.gr>
2019-10-17 00:34:51 +0300

    Update HelloWorld.java

--- Section.3/HelloWorld.java
+++ Section.3/HelloWorld.java
@@ -1,3 +1,8 @@
+//public: is an access modifier 
+//static: is a java keyword that needs an understanding of other concepts for now that we need to have a static for java to find our method to run the code we are going to add
+//void: is yet another java keyword used o indicate that the method will not return anything - more on that later 
+//Code block: is used to define a block of code.it's mandatory to have one in a method declaration and it's have where we will be adding statements to perform certain tasks
+//Statement: This is a complete command to be executed and can include one or more expressions
 public class HelloWorld {
   
   public static void main(String[] args){
