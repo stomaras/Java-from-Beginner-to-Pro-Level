bd1762cfe6879e6b742a885e17ae60ba8e8a47fe
stomaras <sptomaras@yahoo.gr>
2019-10-17 19:33:25 +0300

    Update HelloWorld.java

--- Section.3/HelloWorld.java
+++ Section.3/HelloWorld.java
@@ -6,6 +6,15 @@
 public class HelloWorld {
   
   public static void main(String[] args){
-    System.out.println("Hello World");
+        System.out.println("Hello Tomy");
+        //Variables are a way to store infrmation in our computer.Variables that we define in a program can be accessed by a name we give them
+        //and the computer does the hard work of figuring out where they get stored in the computer Random Access Memory
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        //Declaration Statement used to define a variable by indicating the data type , and the name, and optionally to set the variable to a certain value
+        //Here the type(short for Data Type)is an int,the name is myFirstNumber and the value we are assigning or initializing our variable with is 5.Wea are
+        //declaring a variable of type int with the name myFirstNumber and assigning the value 5 to it
+        //String Literal
+        System.out.println("MyFirstNumber");
+        System.out.println(myFirstNumber);
   }
 }
