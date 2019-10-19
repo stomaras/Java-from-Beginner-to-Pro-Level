576fc283284f1d5be1806d54ddedebe9f8b9721c
stomaras <sptomaras@yahoo.gr>
2019-10-19 18:45:22 +0300

    Create Expression.java

---
+++ Section.3/Expression.java
@@ -0,0 +1,15 @@
+public class Expression {
+    public static void main(String[] args){
+        
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+    }
+}
