1d5f6d80aff341c957f42ccc6407d9c5437278f3
stomaras <sptomaras@yahoo.gr>
2019-10-19 21:02:02 +0300

    Update primitive.java

--- Section.3/primitive.java
+++ Section.3/primitive.java
@@ -1,6 +1,8 @@
 public class primitive {
 
     public static void main(String[] args) {
+
+        //Wrapper classes
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
@@ -9,5 +11,21 @@ public class primitive {
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
         //Underflow
         System.out.println("Busted MIN value = " + (myMinIntValue - 1) );
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinShortValue);
+        System.out.println("Byte Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
     }
 }
