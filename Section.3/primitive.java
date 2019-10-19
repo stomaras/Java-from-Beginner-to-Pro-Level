6720afd8ceace4fe599233c1661ed418431bb5c8
stomaras <sptomaras@yahoo.gr>
2019-10-19 19:17:00 +0300

    Update primitive.java

--- Section.3/primitive.java
+++ Section.3/primitive.java
@@ -1,1 +1,13 @@
+public class primitive {
 
+    public static void main(String[] args) {
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " +myMaxIntValue);
+        //Overflow
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        //Underflow
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1) );
+    }
+}
