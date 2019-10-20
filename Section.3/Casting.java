091830b57b5cbf48e642b94890ad25aafd5d1e48
stomaras <sptomaras@yahoo.gr>
2019-10-21 01:42:13 +0300

    Create Casting.java

---
+++ Section.3/Casting.java
@@ -0,0 +1,36 @@
+public class Casting {
+
+
+    public static void main(String[] args) {
+
+        //Wrapper classes
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        //Overflow
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        //Underflow
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinShortValue);
+        System.out.println("Byte Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMaxShortValue / 2);
+    }
+}
