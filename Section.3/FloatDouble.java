99ff7a0477d056e0dcee01726eac77bbe7365968
stomaras <sptomaras@yahoo.gr>
2019-10-25 23:25:54 +0300

    Update FloatDouble.java

--- Section.3/FloatDouble.java
+++ Section.3/FloatDouble.java
@@ -12,9 +12,24 @@ public class FloatDouble {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+        System.out.println("MyIntValue=" + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        // Convert a given number of pounds to kilograms
+        //STEPS :
+        // 1) Create a variable with the appropriate type to store the number of pounds to be converted to kilograms
+        // 2) Calculate the result i.e the number of kilograms based on the contents of the variable above and store the result in an 2nd appropriate
+        // variable
+        // 3) Print out the result
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted Kilograms = " + convertedKilograms);
+
     }
 
 
