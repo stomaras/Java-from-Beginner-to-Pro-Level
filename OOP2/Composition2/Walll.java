ddaadd64d9a7f15460f1607344b7a831b7d8529c
stomaras <sptomaras@yahoo.gr>
2019-11-14 16:11:09 +0200

    Create Walll.java

---
+++ OOP2/Composition2/Walll.java
@@ -0,0 +1,13 @@
+package OOP2.Composition2;
+
+public class Walll {
+    private String direction;
+
+    public Walll(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
