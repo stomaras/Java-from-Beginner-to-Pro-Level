f8234712dfc67bfc5e248c3b833622ec01e57d2c
stomaras <sptomaras@yahoo.gr>
2019-11-14 13:08:38 +0200

    Create Dog.java

---
+++ OOP1/StaticVars/Dog.java
@@ -0,0 +1,13 @@
+package OOP1.StaticVars;
+public class Dog {
+
+    private static String name;
+
+    public Dog(String name){
+        Dog.name = name;
+    }
+
+    public void printName(){
+        System.out.println("name= " + name);
+    }
+}
