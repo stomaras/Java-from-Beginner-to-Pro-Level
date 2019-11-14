4a80358671b77b7d5b328cffcb339a9654c3a2cb
stomaras <sptomaras@yahoo.gr>
2019-11-14 15:29:10 +0200

    Create Monitor.java

---
+++ OOP2/Composition/Monitor.java
@@ -0,0 +1,35 @@
+package OOP2.Composition;
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResolution;// Monitor has a Resolution this is Composition
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResolution) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResolution = nativeResolution;
+    }
+
+    public void drawPixelAt(int x, int y, String color){
+        System.out.println("Drawing pixel at " + x + "," + y + " in colour " + color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolution() {
+        return nativeResolution;
+    }
+}
