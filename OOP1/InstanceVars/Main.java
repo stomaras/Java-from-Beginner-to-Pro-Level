a1290821f8b3e4bec64b129803e7ce37fa6c89af
stomaras <sptomaras@yahoo.gr>
2019-11-14 13:18:28 +0200

    Create Main.java

---
+++ OOP1/InstanceVars/Main.java
@@ -0,0 +1,10 @@
+package OOP1.InstanceVars;
+public class Main {
+
+    public static void main(String[] args) {
+	    Dog rex = new Dog("rex"); // create instance (rex)
+        Dog flufly = new Dog("flufly"); // create instance (flufly)
+        rex.printName();// prints rex
+        flufly.printName();// prints flufly
+    }
+}
