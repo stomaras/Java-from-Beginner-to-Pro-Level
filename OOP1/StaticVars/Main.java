cf845103bf1f3d67ea4118d68c78c7d13e8772d0
stomaras <sptomaras@yahoo.gr>
2019-11-14 13:07:36 +0200

    Create Main.java

---
+++ OOP1/StaticVars/Main.java
@@ -0,0 +1,11 @@
+package OOP1.StaticVars
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Dog rex = new Dog("rex"); //create instance (rex)
+	    Dog flufly = new Dog("flufly"); //create instance (flufly)
+        rex.printName(); // prints flufly
+        flufly.printName(); // prints flufly
+    }
+}
