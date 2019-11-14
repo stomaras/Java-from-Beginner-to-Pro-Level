3e9c4c7793a90941e49061c928653201e45fce0b
stomaras <sptomaras@yahoo.gr>
2019-11-14 16:11:59 +0200

    Create Ceiling.java

---
+++ OOP2/Composition2/Ceiling.java
@@ -0,0 +1,19 @@
+package OOP2.Composition2;
+
+public class Ceiling {
+    private int height;
+    private int paintedcolor;
+
+    public Ceiling(int height, int paintedcolor) {
+        this.height = height;
+        this.paintedcolor = paintedcolor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedcolor() {
+        return paintedcolor;
+    }
+}
